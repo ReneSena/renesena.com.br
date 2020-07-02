7d6b0b1e289d0f90937b427b7c47237af18930a2
renedocket <rene.sena@docket.com.br>
2020-07-02 02:18:24 -0300

    Criando componente de sobre.

---
+++ src/components/Profile/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Profile = () => (
+    <div className="profile-wrapper">
+        <h1>Rene Sena</h1>
+        <h2>Desenvolvedor Front-end</h2>
+        <p>Eu sou o Rene, gosto muito do mundo web e estou aqui para compartilhar com vocês coisas que estou aprendendo. :)</p>
+    </div>
+)
+
+export default Profile
