be48b3e7d33d0983c3cf3ce389b43b2ac1061a08
renedocket <rene.sena@docket.com.br>
2020-07-08 11:43:05 -0300

    Ajustando caminhos relativos do menu e trocando a foto de profile.

--- src/components/Avatar/index.js
+++ src/components/Avatar/index.js
@@ -9,7 +9,7 @@ const Avatar = () => {
             query {
                 avatarImage: file(relativePath: { eq: "avatar.jpg" }) {
                     childImageSharp {
-                        fixed(width: 60, height: 60) {
+                        fixed(width: 100, height: 100) {
                             ...GatsbyImageSharpFixed
                         }
                     }
