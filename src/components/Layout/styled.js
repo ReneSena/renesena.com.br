0aab73662f52c4703a4c2ec6d6ed68cc19738e51
ReneSena <rene.sena3005@gmail.com>
2020-07-22 17:27:53 -0300

    Adicionando style dentro dos post's + Adicionando imagens dentro dos post's.

--- src/components/Layout/styled.js
+++ src/components/Layout/styled.js
@@ -5,7 +5,7 @@ export const LayoutWrapper = styled.section`
 `
 
 export const LayoutMan = styled.main`
-  background-color: #FFF;
+  background-color: #333;
   min-height: 100vh;
   padding: 0 3.75rem 0 20rem;
   width: 100%;
