9da9ea0d0b3bfff1b083afe79e3140e790b67fff
ReneSena <rene.sena3005@gmail.com>
2020-07-26 18:33:27 -0300

    Criação de component de Search para pesquisa de post's.

---
+++ src/pages/search.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+import Layout from "../components/Layout"
+import SEO from "../components/seo"
+
+import Search from '../components/Search'
+
+const SearchPage = () => (
+  <Layout>
+    <SEO title="About" />
+    <Search />
+  </Layout>
+)
+
+export default SearchPage
