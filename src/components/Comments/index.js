90492bd0e7d9cec3780b91cae6c7ae9dbf347f13
ReneSena <rene.sena3005@gmail.com>
2020-08-24 22:31:23 -0300

    Configurando o disqus com o domínio correto.

--- src/components/Comments/index.js
+++ src/components/Comments/index.js
@@ -5,24 +5,24 @@ import ReactDisqusComments from 'react-disqus-comments';
 import * as S from './styled';
 
 const Comments = ({ url, title }) => {
-    const completeURL = `${url}`
-    return(
-        <S.CommentWrapper>
-            <S.CommentsTitle>Comentários</S.CommentsTitle>
-            <ReactDisqusComments
-                shortname="renesena"
-                identifier={completeURL}
-                title={title}
-                url={completeURL}
-            />
-            );
-        </S.CommentWrapper>
-    );
-}
+	const completeURL = `https://www.renesena.com.br${url}`;
+	return (
+		<S.CommentWrapper>
+			<S.CommentsTitle>Comentários</S.CommentsTitle>
+			<ReactDisqusComments
+				shortname='renesena'
+				identifier={completeURL}
+				title={title}
+				url={completeURL}
+			/>
+			);
+		</S.CommentWrapper>
+	);
+};
 
 Comments.propTypes = {
-    url: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-}
+	url: PropTypes.string.isRequired,
+	title: PropTypes.string.isRequired,
+};
 
 export default Comments;
