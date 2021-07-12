78a1b51212c2a3d16601251fa4ef43b840292fba
ReneSena <rene.sena3005@gmail.com>
2021-07-11 23:54:41 -0300

    Trocando avatar.

--- src/components/Avatar/index.js
+++ src/components/Avatar/index.js
@@ -1,40 +1,42 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { useStaticQuery, graphql } from 'gatsby';
+import React from "react";
+import PropTypes from "prop-types";
+import { useStaticQuery, graphql } from "gatsby";
 
-import * as S from './styled';
+import * as S from "./styled";
 
 const Avatar = ({ sizeDesktop, sizeMobile }) => {
-    const { avatarImage } = useStaticQuery(
-        graphql`
-            query {
-                avatarImage: file(relativePath: { eq: "avatar.jpg" }) {
-                    childImageSharp {
-                        fluid {
-                            ...GatsbyImageSharpFluid
-                        }
-                    }
-                }
-            }
-        `
-    );
+	const { avatarImage } = useStaticQuery(
+		graphql`
+			query {
+				avatarImage: file(relativePath: { eq: "avatar.jpg" }) {
+					childImageSharp {
+						fluid {
+							...GatsbyImageSharpFluid
+						}
+					}
+				}
+			}
+		`
+	);
 
-    return (
-        <S.AvatarWrapper sizeDesktop={sizeDesktop} sizeMobile={sizeMobile}
-            fluid={avatarImage.childImageSharp.fluid}
-            alt='Foto de Rene Sena, criador do site/blog'
-        />
-    );
+	return (
+		<S.AvatarWrapper
+			sizeDesktop={sizeDesktop}
+			sizeMobile={sizeMobile}
+			fluid={avatarImage.childImageSharp.fluid}
+			alt="Foto de Rene Sena, criador do site/blog"
+		/>
+	);
 };
 
 Avatar.propTypes = {
-    sizeDesktop: PropTypes.string,
-    sizeMobile: PropTypes.string
-}
+	sizeDesktop: PropTypes.string,
+	sizeMobile: PropTypes.string,
+};
 
 Avatar.defaultProps = {
-    sizeDesktop: '12.5rem',
-    sizeMobile: '9.5rem'
-}
+	sizeDesktop: "12.5rem",
+	sizeMobile: "9.5rem",
+};
 
 export default Avatar;
